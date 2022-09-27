770bffd3fe014a9686324ed8bca8666690261d5e
movezero <leedy7276@gmail.com>
2022-09-27 22:23:16 +0900

    Start() edited
    앱 시동 시 display 출력하도록 수정

--- aram/Assets/Scripts/EventManager.cs
+++ aram/Assets/Scripts/EventManager.cs
@@ -9,15 +9,18 @@ public class EventManager : MonoBehaviour
     public TMP_InputField userInput;    // string
     public GameObject textDisplay;
     public int myTickets = 159;  //  나의 남은 식수, int
-    public int remainDays = 86;    //  시간에 따라 자동으로 변경 필요
+    public int remainDays = 82;    //  시간에 따라 자동으로 변경 필요
 
     void Start()
     {
         userInput = GameObject.Find("InputField (TMP)").GetComponent<TMP_InputField>(); // InputField 컴포넌트에 접근 
-        myTickets = PlayerPrefs.GetInt("tickets");
-        textDisplay.GetComponent<TextMeshProUGUI>().text = PlayerPrefs.GetString("display", "result");
-        userInput.text = myTickets.ToString();
-        Test();
+        myTickets = PlayerPrefs.GetInt("tickets");  // 이전에 실행했을 때 저장했던 myTickets 값을 불러옴
+        textDisplay.GetComponent<TextMeshProUGUI>().text = PlayerPrefs.GetString("display");    // PlayerPrefs 에 저장된 문자열 불러옴
+        userInput.text = myTickets.ToString();  // InputField 에 myTickets값을 보이게 하기 위해 문자열로 변경 후 대입
+
+        Debug.Log(PlayerPrefs.GetString("display"));
+        Debug.Log(PlayerPrefs.GetInt("tickets"));
+        //Test();
     }
     public void StoreTicket()
     {
@@ -25,7 +28,7 @@ public class EventManager : MonoBehaviour
         myTickets = int.Parse(userInput.text);
         float meal_per_day = myTickets / ((remainDays * 3 + 1) / 3.0f); // 계산식
         string result = string.Format("하루에 {0:0.##}끼 씩 먹으면 다 먹을 수 있습니다.", meal_per_day);    // 소수점 두 번째 자리에서 반올림
-        Save();
+        Save(result);
         textDisplay.GetComponent<TextMeshProUGUI>().text = result;  // 계산 결과를 화면에 나타내기 
     }
     public void Plus() // + 버튼 클릭 시
@@ -39,22 +42,12 @@ public class EventManager : MonoBehaviour
         myTickets -= 1;
         userInput.text = myTickets.ToString();  // 몇 끼 입력했는지 화면에 나타내기
         StoreTicket();
-
     }
-    public void Save()
+    public void Save(string result1)
     {
         PlayerPrefs.SetInt("tickets", myTickets);   // 메모리에 정수값 저장
-        PlayerPrefs.SetString("display", "result");    // 메모리에 문자열 저장
+        PlayerPrefs.SetString("display", result1);    // 메모리에 문자열 저장
         PlayerPrefs.Save();     // 수정된 모든 preferences를 파일(드라이브)에 저장한다.
-    }
-
-    public void Test()  // 명시적 형변환 테스트
-    {
-        int num = 50000;
-        short smallNum = (short)num;
-        char stringNum = (char)num;
-        Debug.Log(smallNum);
-        Debug.Log(stringNum);
     }
 
     public void AllDelete()    // 저장되어 있는 모든 데이터 삭제하기   PlayerPrefs 초기화 
@@ -70,6 +63,30 @@ public class EventManager : MonoBehaviour
     {
         PlayerPrefs.DeleteKey("display");
     }
+
+
+
+
+
+
+    public void Test()  // 명시적 형변환 테스트
+    {
+        int num = 50000;
+        short smallNum = (short)num;
+        char stringNum = (char)num;
+        Debug.Log(smallNum);
+        Debug.Log(stringNum);
+
+        string a = "1234";
+        //int b = (int)a;     // 오류
+        int b = int.Parse(a);   // string 형식의 a를 int로 변환
+
+        string c = "3.14";
+        //double d = (double)c;
+        double d = double.Parse(c);
+        float f = float.Parse(c);
+    }
+
 }
 
 // int remain_days = 89;   // 종강까지 남은 일수 12월21일 아침까지 운영
